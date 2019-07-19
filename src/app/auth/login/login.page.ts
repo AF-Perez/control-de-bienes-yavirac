e11b7b2ff88a7ec1f80b69e54f04f801fc39121d
Andres <fercholduliga@hotmail.com>
2019-07-19 14:59:12 -0500

    7192019

--- src/app/auth/login/login.page.ts
+++ src/app/auth/login/login.page.ts
@@ -1,6 +1,14 @@
+import { TestBed } from '@angular/core/testing';
 import { Component, OnInit } from '@angular/core';
 import { Router } from  "@angular/router";
 import { AuthService } from '../auth.service';
+//import { IonicPage, AlertController } from 'ionic-angular';
+import { Validators, FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { PasswordValidator } from '../../validadores/password.validador';
+
+import { AlertController } from '@ionic/angular';
+
+
 
 @Component({
   selector: 'app-login',
@@ -9,25 +17,90 @@ import { AuthService } from '../auth.service';
 })
 export class LoginPage implements OnInit {
 
-  constructor(private  authService:  AuthService, private  router:  Router) { }
+  // variables de clase
+  validations_form: FormGroup;
+  matching_passwords_group: FormGroup;
+
+  constructor(
+    private  authService:  AuthService, 
+    private  router:  Router,
+    public formBuilder: FormBuilder,
+  ) {
+ 
+  }
+
 
   ngOnInit() {
+    this.validations_form = this.formBuilder.group({
+        email: new FormControl('', Validators.compose([
+          Validators.required,
+          Validators.pattern('^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$')
+      ])),
+      matching_passwords: this.matching_passwords_group,
+     
+    });
+
+    this.matching_passwords_group = new FormGroup({
+      password: new FormControl('', Validators.compose([
+        Validators.minLength(5),
+        Validators.required,
+        Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]+$')
+      ])),
+      confirm_password: new FormControl('', Validators.required)
+    }, (formGroup: FormGroup) => {
+      return PasswordValidator.areEqual(formGroup);
+    });
 
   }
 
+
+  // loguear al usuario
   login(formulario){
-    //validacion
-    let correo = formulario.value.email;
-    let clave = formulario.value.password;
-    
-    console.log("su correo es = " + correo);
-    console.log("su contraseña es = " + clave);
     // loguear
+    console.log(formulario);
     this.authService.logear(formulario.value).subscribe((res)=>{
       this.router.navigateByUrl('usuarios');
     });
   }
 
 
-
+  validation_messages = {
+    'username': [
+      { type: 'required', message: 'Username is required.' },
+      { type: 'minlength', message: 'Username must be at least 5 characters long.' },
+      { type: 'maxlength', message: 'Username cannot be more than 25 characters long.' },
+      { type: 'pattern', message: 'Your username must contain only numbers and letters.' },
+      { type: 'validUsername', message: 'Your username has already been taken.' }
+    ],
+    'name': [
+      { type: 'required', message: 'Name is required.' }
+    ],
+    'lastname': [
+      { type: 'required', message: 'Last name is required.' }
+    ],
+    'email': [
+      { type: 'required', mensaje: 'Email es requerido.' },
+      { type: 'pattern', mensaje: 'por favor ingrese un E-mail valido.' }
+    ],
+    'phone': [
+      { type: 'required', message: 'Phone is required.' },
+      { type: 'validCountryPhone', message: 'The phone is incorrect for the selected country.' }
+    ],
+    'password': [
+      { type: 'required', message: 'Password is required.' },
+      { type: 'minlength', message: 'Password must be at least 5 characters long.' },
+      { type: 'pattern', message: 'Your password must contain at least one uppercase, one lowercase, and one number.' }
+    ],
+    'confirm_password': [
+      { type: 'required', message: 'Confirm password is required.' }
+    ],
+    'matching_passwords': [
+      { type: 'areEqual', message: 'Password mismatch.' }
+    ],
+    'terms': [
+      { type: 'pattern', message: 'You must accept terms and conditions.' }
+    ],
+  };
+  
+  
 }
