559a255ae00d0d556f5f69631d2e2e460597a23c
Andres <fercholduliga@hotmail.com>
2019-09-20 15:13:50 -0500

    formulario

--- src/app/pantallas/formularios/crear-bienes/crear-bienes.page.ts
+++ src/app/pantallas/formularios/crear-bienes/crear-bienes.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Validators, FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UsernameValidator } from 'src/app/validadores/usuario.validador';
 
 @Component({
   selector: 'app-crear-bienes',
@@ -7,9 +10,78 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CrearBienesPage implements OnInit {
 
-  constructor() { }
+  validations_form: FormGroup;
+ 
+  genders: Array<string>;
+
+  constructor(
+    public formBuilder: FormBuilder,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+
+    this.genders = [
+      "Tecnologicos",
+      "Muebles",
+      "libros",
+    ];
+
+    this.validations_form = this.formBuilder.group({
+      username: new FormControl('', Validators.compose([
+        UsernameValidator.validUsername,
+        Validators.maxLength(25),
+        Validators.minLength(5),
+        Validators.pattern('^(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9]+$'),
+        Validators.required
+      ])),
+      nombre: new FormControl('', Validators.required),
+      gender: new FormControl('', Validators.required),
+      terms: new FormControl(true, Validators.pattern('true'))
+    });
+  }
+
+  validation_messages = {
+    'username': [
+      { type: 'required', message: 'Username is required.' },
+      { type: 'minlength', message: 'Username must be at least 5 characters long.' },
+      { type: 'maxlength', message: 'Username cannot be more than 25 characters long.' },
+      { type: 'pattern', message: 'Your username must contain only numbers and letters.' },
+      { type: 'validUsername', message: 'Your username has already been taken.' }
+    ],
+    'name': [
+      { type: 'required', message: 'Name is required.' }
+    ],
+    'lastname': [
+      { type: 'required', message: 'Last name is required.' }
+    ],
+    'email': [
+      { type: 'required', message: 'Email is required.' },
+      { type: 'pattern', message: 'Please wnter a valid email.' }
+    ],
+    'phone': [
+      { type: 'required', message: 'Phone is required.' },
+      { type: 'validCountryPhone', message: 'The phone is incorrect for the selected country.' }
+    ],
+    'password': [
+      { type: 'required', message: 'Password is required.' },
+      { type: 'minlength', message: 'Password must be at least 5 characters long.' },
+      { type: 'pattern', message: 'Your password must contain at least one uppercase, one lowercase, and one number.' }
+    ],
+    'confirm_password': [
+      { type: 'required', message: 'Confirm password is required.' }
+    ],
+    'matching_passwords': [
+      { type: 'areEqual', message: 'Password mismatch.' }
+    ],
+    'terms': [
+      { type: 'pattern', message: 'You must accept terms and conditions.' }
+    ],
+  };
+
+  onSubmit(values){
+    console.log(values);
+    this.router.navigate(["/user"]);
   }
 
 }
