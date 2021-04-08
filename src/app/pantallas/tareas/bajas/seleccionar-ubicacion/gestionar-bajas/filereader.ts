5950d590eb29d123a590a3f06fc172d4029dbe66
Your Name <you@example.com>
2021-04-08 11:25:50 -0500

    modulo bajas cont

---
+++ src/app/pantallas/tareas/bajas/seleccionar-ubicacion/gestionar-bajas/filereader.ts
@@ -0,0 +1,15 @@
+import { Observable, fromEvent } from 'rxjs';
+ 
+export class FileReaderObservable {
+  public readonly reader = new FileReader();
+  private getEvent<T>( name: string ): Observable<T> {
+    return fromEvent<T>( this.reader, name );
+  }
+ 
+  get onAbort() { return this.getEvent<ProgressEvent>( 'abort' ) } // 中断時
+  get onError() { return this.getEvent<ProgressEvent>( 'error' ) } // エラー時
+  get onLoad() { return this.getEvent<ProgressEvent>( 'load' ) } // 読出し成功時
+  get onLoadEnd() { return this.getEvent<ProgressEvent>( 'loadend' ) } // 成否を問わず読出し完了時
+  get onLoadStart() { return this.getEvent<ProgressEvent>( 'loadstart' ) } // 読出し開始時
+  get onProgress() { return this.getEvent<ProgressEvent>( 'progress' ) } // 読出し中(何回か発生)
+}
--- src/app/pantallas/tareas/bajas/seleccionar-ubicacion/gestionar-bajas/gestionar-bajas.page.ts
+++ src/app/pantallas/tareas/bajas/seleccionar-ubicacion/gestionar-bajas/gestionar-bajas.page.ts
@@ -6,9 +6,11 @@ import { AlertController, ModalController, ToastController } from '@ionic/angula
 import { ModalBajasPage } from './modal-bajas.page';
 import { Location } from '@angular/common';
 import { Baja } from 'src/app/models/baja.model';
-import { Subscription, forkJoin } from 'rxjs';
+import { Subscription, forkJoin, Observable, from, Observer, merge, throwError } from 'rxjs';
 import { File, FileEntry } from '@ionic-native/file/ngx';
 import { LoadingController } from '@ionic/angular';
+import { FileReaderObservable } from './filereader';
+import { flatMap, map, takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-gestionar-bajas',
@@ -24,6 +26,7 @@ export class GestionarBajasPage implements OnInit {
   idAsignacion: any;
   bajas: Baja[] = [];
   private bajasSub: Subscription;
+  bajasRequests: [];
 
   constructor(
     private route: ActivatedRoute,
@@ -48,21 +51,10 @@ export class GestionarBajasPage implements OnInit {
 
   ngOnInit() {
     this.obtenerFechaActual();
-    // this.obtenerBienes();
     this.servicioTareas.bajasTarea.subscribe(bajas => {
       console.log('bajas en gestionar-bajas', bajas);
       this.bajas = bajas;
     });
-  }
-
-  obtenerBienes() {
-    this.servicioBienes.traerBienesDeUbicacion(this.ubicacion)
-      .subscribe(bienes => {
-        this.bienes = bienes.filter(bien => {
-          return bien.is_baja !== 1;
-        });
-        // this.bienes = bienes;
-      });
   }
 
   obtenerFechaActual() {
@@ -89,35 +81,6 @@ export class GestionarBajasPage implements OnInit {
     return await modal.present();
   }
 
-  ingresarTarea(idTarea) {
-    this.loadingController
-      .create({
-        message: 'Procesando solicitud...'
-      })
-      .then(loadingEl => {
-
-        loadingEl.present();
-
-        let promises = [];
-
-        console.log(this.bajas);
-
-        this.bajas.forEach((baja) => {
-          let tempPromise = this.file.resolveLocalFilesystemUrl(baja.imgData.filePath);
-          promises.push(tempPromise);
-        });
-
-        // .then(entry => {
-        //   (<FileEntry>entry).file(file => this.readFile(file, baja));
-        // });
-
-        forkJoin(promises).subscribe(val => {
-         console.log(val);
-        });
-
-      });
-  }
-
   // ingresarTarea(idTarea) {
   //   this.loadingController
   //     .create({
@@ -127,47 +90,198 @@ export class GestionarBajasPage implements OnInit {
 
   //       loadingEl.present();
 
+  //       let promises = [];
+
+  //       console.log(this.bajas);
+
   //       this.bajas.forEach((baja) => {
-  //         this.file.resolveLocalFilesystemUrl(baja.imgData.filePath)
-  //           .then(entry => {
-  //             (<FileEntry>entry).file(file => this.readFile(file, baja));
-  //           });
+  //         let tempPromise = this.file.resolveLocalFilesystemUrl(baja.imgData.filePath);
+  //         promises.push(tempPromise);
   //       });
 
-  //       setTimeout(() => {
-  //         console.log('All done!');
-  //         this.servicioTareas.completarTarea(idTarea).subscribe(respuesta => {
-  //           this.tareasService.removerTarea(this.idAsignacion);
-  //           loadingEl.dismiss();
-  //           this._location.back();
-  //         });
-  //       }, 3000);
+  //       // .then(entry => {
+  //       //   (<FileEntry>entry).file(file => this.readFile(file, baja));
+  //       // });
+
+  //       forkJoin(promises).subscribe(val => {
+  //         console.log(val);
+  //       });
+
   //     });
   // }
 
-  readFile(file: any, baja: Baja) {
-    const reader = new FileReader();
-    reader.onload = () => {
-      console.log('file loaded');
-      const imgBlob = new Blob([reader.result], { type: file.type });
-      const imgData = { blob: imgBlob, name: file.name };
-      this.servicioTareas.submitBaja(
-        baja.codigoBien,
-        this.idAsignacion,
-        baja.motivoBaja,
-        imgData,
-      ).subscribe((_) => {
-        console.log('in readfile')
-        this.deleteImage(baja.imgData);
-        this.servicioTareas.removerBaja(baja.codigoBien);
-      },
-        (err) => {
-          console.error("Error " + err)
-        }
-      )
-    };
-    reader.readAsArrayBuffer(file);
+  async getFile(fileEntry) {
+    try {
+      return await new Promise((resolve, reject) => fileEntry.file(resolve, reject));
+    } catch (err) {
+      console.log(err);
+    }
   }
+
+  ingresarTarea(idTarea) {
+    this.loadingController
+      .create({
+        message: 'Procesando solicitud...'
+      })
+      .then(loadingEl => {
+
+        loadingEl.present();
+        console.log('this.bajas :>> ', this.bajas);
+
+        let resolvLocalFSObsArr = [];
+
+        this.bajas.forEach((baja) => {
+          resolvLocalFSObsArr.push(from(this.file.resolveLocalFilesystemUrl(baja.imgData.filePath)));
+          // .then(entry => {
+          //   // (<FileEntry>entry).file(file => this.readFile(file, baja));
+          // });
+        });
+
+        forkJoin(resolvLocalFSObsArr)
+          .subscribe((fentries) => {
+            console.log('res :>> ', fentries);
+            let getFileObs = fentries.map(fe => {
+              return from(this.getFile(fe));
+            });
+            console.log('getFileObs :>> ', getFileObs);
+            forkJoin(getFileObs)
+              .subscribe((files) => {
+                console.log('files :>> ', files);
+                // let readFileObs = files.map(file => {
+                //   // return this.readFile(file);
+                //   return from(this.readFile(file));
+                // });
+
+                // console.log('readFileObs :>> ', readFileObs);
+
+                this.readFiles(files);
+               
+
+                // forkJoin(readFileObs).subscribe((imgData) => {
+                //   console.log('imgData :>> ', imgData);
+                // });
+              });
+          });
+
+        setTimeout(() => {
+          loadingEl.dismiss();
+
+          // console.log('All done!');
+          // this.servicioTareas.completarTarea(idTarea).subscribe(respuesta => {
+          //   this.tareasService.removerTarea(this.idAsignacion);
+          //   loadingEl.dismiss();
+          //   this._location.back();
+          // });
+        }, 4000);
+      });
+  }
+
+  readFiles( files: any[] ) {
+    from( files ).pipe( this.readAsText() ).subscribe( 
+      ( res ) => console.log( res ),
+      ( err )=> console.log( err ) ,
+      ()=>console.log( 'end' )
+    );
+  }
+
+  readAsText = () => ( src: Observable<any> ) => {
+    return src.pipe(
+      flatMap( file => {
+        const obs = new FileReaderObservable();
+        // 中断かエラーが生じたら例外を投げるObservable
+        const failed = merge( obs.onAbort, obs.onError ).pipe( flatMap( evt => throwError( `Cannot read file. ${file.name}.` ) ) );
+   
+        // ファイルの読出しが成功したら結果を文字列として返すObservable
+        // Typescript 3.5.*での問題
+        // https://github.com/microsoft/TypeScript/issues/25510
+        // eventtarget.resultが消えてしまっているためany化している
+        const data = obs.onLoad.pipe( map( evt => ( evt as any ).target.result as string ) );
+         
+        // 読出し開始
+        obs.reader.readAsText( file );
+   
+        // mergeで、読出しと例外の発生を監視するObservableを統合し、
+        // takeUntilで成否を問わず読出しが完了したら購読を終了するようにする。
+        return merge( data, failed ).pipe( takeUntil( obs.onLoadEnd ) );
+      } )
+    );
+  }
+
+
+  readFile(file) {
+    return new Promise((resolve, reject) => {
+      let reader = new FileReader();
+  
+      reader.onload = () => {
+        resolve(reader.result);
+      };
+  
+      reader.onerror = reject;
+  
+      reader.readAsArrayBuffer(file);
+    })
+  }
+  
+
+  // readFile(file): Observable<any> {
+
+  //   console.log('alfdjlkj :>> ');
+
+  //   const reader = new FileReader();
+
+  //   return new Observable((observer: Observer<any>) => {
+  //     reader.readAsArrayBuffer(file);
+  //     reader.onload = () => {
+  //       const imgBlob = new Blob([reader.result], { type: file.type });
+  //       const imgData = { blob: imgBlob, name: file.name };
+  //       observer.next(imgData);
+  //       observer.complete();
+  //     };
+  //     reader.onerror = () => {
+  //       observer.error({ error: { name, errorMessage: 'INVALID_FILE' } });
+  //     };
+  //   });
+
+  // }
+
+  // readFile(file: any) {
+  //   const reader = new FileReader();
+  //   reader.onload = () => {
+  //     const imgBlob = new Blob([reader.result], { type: file.type });
+  //     const imgData = { blob: imgBlob, name: file.name };
+  //     // this.servicioTareas.submitBaja(
+  //     //   baja.codigoBien,
+  //     //   this.idAsignacion,
+  //     //   baja.motivoBaja,
+  //     //   imgData,
+  //     // )
+  //   };
+  //   reader.readAsArrayBuffer(file);
+  // }
+
+  // readFile(file: any, baja: Baja) {
+  //   const reader = new FileReader();
+  //   reader.onload = () => {
+  //     console.log('file loaded');
+  //     const imgBlob = new Blob([reader.result], { type: file.type });
+  //     const imgData = { blob: imgBlob, name: file.name };
+  //     this.servicioTareas.submitBaja(
+  //       baja.codigoBien,
+  //       this.idAsignacion,
+  //       baja.motivoBaja,
+  //       imgData,
+  //     ).subscribe((_) => {
+  //       console.log('in readfile')
+  //       this.deleteImage(baja.imgData);
+  //       this.servicioTareas.removerBaja(baja.codigoBien);
+  //     },
+  //       (err) => {
+  //         console.error("Error " + err)
+  //       }
+  //     )
+  //   };
+  //   reader.readAsArrayBuffer(file);
+  // }
 
   // startUpload(imgEntry) {
   //   this.file.resolveLocalFilesystemUrl(imgEntry.filePath)
